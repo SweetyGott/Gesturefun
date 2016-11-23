2742f61959a4e258bbc251f9f3746e4c454499c4
Lukas Vogel <lukas@vogelnest.org>
2016-11-23 16:44:54 +0100

    added gesture recognition for fist

--- app.js
+++ app.js
@@ -9,16 +9,32 @@ Leap.loop(function (frame) {
     frame.hands.forEach(function (hand, index) {
 
         if (brushSizeGestureRecognized(hand)) {
-            document.getElementById('bla').textContent = "Brush size gesture recognized!";
             curScaling = convertRange(hand.indexFinger.tipPosition[1],[0,700],[0.1,3]);
+            setDisplay('brushSize',true)
         } else {
-            document.getElementById('bla').textContent = ""
+            setDisplay('brushSize',false)
         }
+
+        if (openDrawingToolsGestureRecognized(hand)) {
+            setDisplay('toolbox',true)
+        } else {
+            setDisplay('toolbox',false)
+        }
+
         drawingApp.transformRectangle(hand.screenPosition(), hand.roll(), curScaling);
     });
 
 }).use('screenPosition', {scale: 0.25});
 
+
+function setDisplay(elementId,isActive) {
+    if (isActive) {
+        document.getElementById(elementId).style.backgroundColor = "#00FF00";
+    } else {
+        document.getElementById(elementId).style.backgroundColor = "#FFFFFF";
+    }
+
+}
 function convertRange(value, fromRange, toRange) {
     return ( value - fromRange[0] ) * ( toRange[1] - toRange[0] ) / ( fromRange[1] - fromRange[0] ) + toRange[0];
 }
@@ -65,9 +81,20 @@ function brushSizeGestureRecognized(hand) {
     var pinkyDirection = hand.pinky.direction;
 
     var approxEqualLocation = function (a, b) {
-        return math.distance(a,b) < 90;
+        return math.distance(a,b) < 50;
     };
 
     return approxEqualLocation(thumbPosition, pinkyPosition) &&
         pinkyDirection[1] < -0.5;
+}
+
+function openDrawingToolsGestureRecognized(hand) {
+    var palmPosition = hand.palmPosition;
+
+    for (var i = 1; i < 5; i++) {
+        if (math.distance(palmPosition,hand.fingers[i].tipPosition) > 50) {
+            return false;
+        }
+    }
+    return true;
 }
